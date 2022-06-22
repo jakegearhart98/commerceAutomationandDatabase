b3c5d753d61ec9102db703cf55101002a48b8d2e
jakegearhart98 <jakegearhart98@gmail.com>
2022-06-22 12:24:19 -0400

    Changed parsing technique to beautifulsoup, alot faster. Only changed in ebayBot.py

--- ebay/ebayBot.py
+++ ebay/ebayBot.py
@@ -1,3 +1,4 @@
+from itertools import count
 from lib2to3.pgen2 import driver
 from webbrowser import Chrome
 from numpy import product
@@ -11,69 +12,94 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import time, os
 from lxml import etree
+from selenium.webdriver.chrome.options import Options
 
 #driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
-
+options = Options()
+options.headless = True
 #column_names = ["Product Name", "Amazon Price"]
 ebayHomeURL = "https://www.ebay.com/"
 
-df = pd.read_csv('name_and_price.txt')
-df.columns = ["Product Name", "Amazon Price"]
+df = pd.read_csv('productNames_export.csv')
+df.columns = ["Product Name", "Amazon Price", "Product URL"]
+
+#obtain length of columns of dataframe
+dataFrameHeight = df.shape[0]
+
+
 productDataColumnNames = ["Product Name", "Amazon Price", "eBay Average", "Percentage Sold", "Total Amount of Listings"]
 productDataFrame = pd.DataFrame(columns = productDataColumnNames)
-print(productDataFrame)
+
 item_prices = []
 #print(df.iloc[0][0])
 
+
 driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
-
-
- #obtain ebay home page
-
-#time.sleep(1)
-count = 0
+count = 1
 driver.get(ebayHomeURL)
 
-while count < 14:
-    print("Value of count %i" %count)
-    if count == 0:
-        #search item into search bar and press enter
-        element = driver.find_element(By.ID, "gh-ac").send_keys(df.iloc[count][0], Keys.ENTER)
-    if count != 0:
-        driver.find_element(By.XPATH, '//*[@id="gh-ac"]').clear()
-        element = driver.find_element(By.XPATH, '//*[@id="gh-ac"]').send_keys(df.iloc[count][0], Keys.ENTER)
-    time.sleep(2)
+driver.find_element(By.ID, "gh-ac").send_keys(df.iloc[count][0], Keys.ENTER)
+#time.sleep(2)
+#clicks completed listing, only needs to be activated once at the beginning then ebay doesn't switch you out of that setting
+completedItemsBar = driver.find_element(By.XPATH, "/html/body/div[5]/div[3]")
+completedItemsBar.find_element(By.NAME, 'LH_Complete').click()
 
-    if count == 0:
-        #clicks completed items
-        driver.find_element(By.XPATH, '//*[@id="x-refine__group__8"]/ul/li[4]/div/a').click()
+time.sleep(1)
 
-    time.sleep(1)
+######NEED TO START LOOP, COMPLETED LISTING IS CLICKED, HAVE LOOP BE BASED ON LENGTH OF PRODUCTdATAFRAME
 
-    pageSource = driver.page_source
-    soup = BeautifulSoup(pageSource, features = 'lxml')
+def scanEbayPage(htmlPage):
+    ##WORK ON THIS METHOD NEXT, SCAN EBAY PAGE AND GET ALL THE DATA, CALL THIS METHOD WITHIN WHILE LOOP
+    pass
 
-    #I got this bit from geeksforgeeks https://www.geeksforgeeks.org/how-to-use-xpath-with-beautifulsoup/
-    #dom = etree.HTML(str(soup))
-    #product_list = driver.find_elements(By.XPATH, "//*[@id='srp-river-results']/ul/li")
-    listCount = 2
+
+while count < dataFrameHeight:
     
-    while listCount < 70:
-        #try:
-            #product = driver.find_element(By.XPATH, '//*[@id="srp-river-results"]/ul/li[%s]' %(listCount))
-            #itemSold = driver.find_element(By.XPATH, '//*[@id="srp-river-results"]/ul/li[%s]/div/div[2]/div[1]/div/span[l]' %(listCount))
-            productSoup = BeautifulSoup(driver.find_element(By.XPATH, '//*[@id="srp-river-results"]/ul/li[%s]' %(listCount)), 'html.parser')
-            
-            print(productSoup)
-            #print(itemSold)
-            print(listCount)
-            listCount += 1
+    #this finds the search bar
+    driver.find_element(By.ID, 'gh-ac').clear()
+    driver.find_element(By.ID, 'gh-ac').send_keys(df.iloc[count][0], Keys.ENTER)
+    time.sleep(0.5)
+    ###################
 
-        #except:
-        #    listCount = 71
-            pass
+    print('completed listing loading, starting your code')
 
-            
-    #time.sleep(3)
+    soup_level1 = BeautifulSoup(driver.page_source, 'html.parser')
+    middleSection = soup_level1.find(id ='srp-river-results')
+    itemList = middleSection.find_all("li", class_ = "s-item s-item__pl-on-bottom")
     
+    for eachItem in itemList:
+        dateSold = eachItem.find("span", class_='POSITIVE')
+        try:
+            print(dateSold.text)
+        except:
+            dateSold = False
+            print(dateSold)
+    #soup_level2 = BeautifulSoup(, 'html.parser')
+
+    """
+    for eachProduct in productCells:
+        #print(eachProduct)
+        soup = BeautifulSoup(eachProduct, '')
+        print(soup)
+    
+    """
+
+    """
+    for eachProduct in productCells:
+        #print(eachProduct.text + '\n\n')
+        itemSold = eachProduct.find_element(By.CLASS_NAME, 's-item__title--tagblock ')
+        dateSoldSection = itemSold.find_element(By.CLASS_NAME, 'POSITIVE')
+        dateList = (dateSoldSection.text).split()
+        del(dateList[0])
+        dateSold = " ".join(dateList)
+        print(dateSold)
+        boolIemSold = True
+    """
+
+        #soldItemString.splitlines()
+        #print(soldItemString[0])
+    #print(productCell)
+    #print(driver.find_element(By.ID, 'srp-river-main'))
     count += 1
+
+    print(count)
